f0c7fc2a24e9e4c462fce3594b3b11201b1f15bb
Arnold Noronha <arnold@tdrhq.com>
2024-03-01 12:16:55 -0500

    Add three targets

---
+++ target1/gen.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+set -x
+
+cd `dirname $0`
+
+mkdir screenshots || true
+MAGICK=""
+env
+
+if [ `git rev-parse --abbrev-ref HEAD` = master ] ; then
+    sleep 0
+
+fi
+
+if magick --help ; then
+    MAGICK=magick
+fi
+
+echo Before convert call
+
+$MAGICK convert --version
+
+$MAGICK convert -size 360x360 xc:white -font "FreeMono" -pointsize 12 -fill black -stroke black -draw @ascii.txt screenshots/image.png
+
+echo After convert call
