8ad12079b9765287c6ed842fdf1b54c8b642fb1d
Arnold Noronha <arnold@tdrhq.com>
2024-03-01 12:51:26 -0500

    Run the targets

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -2,4 +2,13 @@
 set -x
 set -e
 
+maybe_run_target() {
+    cd $1 && ./gen.sh
+    ~/screenshotbot/recorder --directory screenshots --channel "monorepo-example/$1"
+}
+
+for target in target1 target2 target3 ; do
+    maybe_run_target(target)
+done
+
 echo "run-all-tests done"
